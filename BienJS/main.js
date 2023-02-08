2b9d8ea466f7425d09a3f19fdb848874f8d4b038
HanHan195 <henrytran.195@gmail.com>
2023-02-08 15:08:27 +0700

    baitap

---
+++ BienJS/main.js
@@ -0,0 +1,25 @@
+let i = 10
+let f = 20.5
+let b = true
+let s = "Hà Nội"
+
+document.write('i = ' + i)
+document.write('</br>')
+document.write('f = ' + 20.5)
+document.write('</br>')
+document.write('b = ' + true)
+document.write('</br>')
+document.write('s = ' + s)
+document.write('</br>')
+document.write('</br>')
+document.write('</br>')
+ document.write('diện tích hình chữ nhật')
+ document.write('</br>')
+let width = 20
+let hieght = 10
+let area = width * hieght
+document.write('area =' +area)
+document.write('</br>')
+document.write('</br>')
+document.write('tìm bội số')
+
---
+++ BienJS/math.js
@@ -0,0 +1,8 @@
+let a = prompt('10');
+let b = prompt('2');
+// đổi sang dạng number
+  a = parseInt(a)
+  b = parseInt(b)
+  
+alert('a % b');
+
