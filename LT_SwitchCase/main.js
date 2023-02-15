d4772772e556b543512fddaced09e735d05f8c62
HanHan195 <henrytran.195@gmail.com>
2023-02-15 10:47:12 +0700

    Lt_SwitchCase_Toantubangoi

---
+++ LT_SwitchCase/main.js
@@ -0,0 +1,38 @@
+// Switch Case:
+
+// let TN = +prompt("MỨc lương:");
+// switch (true) {
+//     case (TN <= 5):
+//        document.write("Bậc 1.");
+//         break;
+//     case (TN <= 10):
+//        document.write("Bậc 2.");
+//         break;
+//     case (TN <= 18):
+//        document.write("Bậc 3.");
+//         break;
+//     case (TN <= 32):
+//        document.write("Bậc 4.");
+//         break;
+//     case (TN <= 52):
+//        document.write("Bậc 5.");
+//         break;
+//     case (TN <= 80):
+//        document.write("Bậc 6.");
+//         break;
+//     case (TN > 80):
+//        document.write("Bậc 7.");
+//         break;
+// }
+
+// Toán tử ba ngôi
+
+let TN = 30;
+TN <= 5 ? console.log("Bậc 1") : 
+    TN <= 10 ? console.log("Bậc 2") : 
+        TN <= 18 ? console.log("Bậc 3") : 
+            TN <= 32 ? console.log("Bậc 4") : 
+                TN <= 52 ? console.log("Bậc 5") : 
+                    TN <= 80 ? console.log("Bậc 6") : console.log("Bậc 7") ; 
+
+                         
