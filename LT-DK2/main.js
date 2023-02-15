7ef947874d08c373a2a77c3258229b4d9bc69b8c
HanHan195 <henrytran.195@gmail.com>
2023-02-15 16:47:21 +0700

    LT-DK2

---
+++ LT-DK2/main.js
@@ -0,0 +1,27 @@
+// let score = +prompt("Nhập điểm:")
+// score >= 9 ? document.write("A") : 
+//     score >= 8 ? document.write("B") :
+//         score >= 7 ? document.write("C") : 
+//             score >= 6 ? document.write("D") : document.write("F");
+
+
+// 
+// let a = 10
+// let b = 5 
+// document.write(`(${a}^2 - ${b}^2) = (${a} + ${b}) * (${a} - ${b}) `);
+
+
+
+
+// switch (true) {
+//     case a > 0 && b > 0 && c > 0:
+//         case (a + b > c):
+//             case (b + c > a):
+//                 case (a + c > b):
+//                     console.log("Đây là hình tam giác.");
+//                     break;
+//                     default: 
+//                     console.log("Đây không phải hình tam giác!")
+// }
+
+
